92d175d594f36ae36641e257f68bbdbc9a274f27
akitomonam <akitomonam@gmail.com>
2024-01-13 19:00:13 +0900

    全データ作成用に修正

--- Data/data_prepare_zero-shot_MultiWOZ20.py
+++ Data/data_prepare_zero-shot_MultiWOZ20.py
@@ -16,22 +16,22 @@ def Template1(dial_text):
 
     if "Possible Values" in dial[1]:
         dial2 = dial[1].split(" [Possible Values] ")
-        
+
         input1 = input1 + "[domain] " + dial2[0] + " "
         if random.random() > 0.5:
             input1 = input1 + "This slot is categorical and you can only choose from the following available values: "
             input1 = input1 + dial2[1] + ". "
-        
+
     else:
         input1 = input1 + "[domain] " + dial[1] + " "
 
     input1 = input1 + "If the slot is not mentioned in the dialogue, just return NONE. \n "
-    
+
     if random.random() > 0.5:
         input1 = input1 + "So the value of slot <"+ d_name+ "-" + s_name +"> is \n"
     else:
         input1 = input1 + "So the value of slot <"+ d_name+ "-" + s_name +"> is "
-    
+
     output1 = dial_text['state']
     return input1, output1
 
@@ -43,12 +43,12 @@ def Template2(dial_text):
 
     if "Possible Values" in dial[1]:
         dial2 = dial[1].split(" [Possible Values] ")
-        
+
         input1 = input1 + "[domain] " + dial2[0] + " "
         if random.random() > 0.5:
             input1 = input1 + "This slot is categorical and you can only choose from the following available values: "
             input1 = input1 + dial2[1] + ". "
-        
+
     else:
         input1 = input1 + "[domain] " + dial[1] + " "
 
@@ -57,10 +57,10 @@ def Template2(dial_text):
         input1 = input1 + "So the value of slot <"+ d_name+ "-" + s_name +"> is \n"
     else:
         input1 = input1 + "So the value of slot <"+ d_name+ "-" + s_name +"> is "
-    
+
     output1 = dial_text['state']
     return input1, output1
-  
+
 
 def Template3(dial_text):
     dial = dial_text['dialogue'].split(" [domain] ")
@@ -82,21 +82,21 @@ if __name__ == '__main__':
 
     except_domain = "attraction" # delete this domain from traning set
     activate_number = 471912
-    
+
     except_number = 0
     original_number = 0
-    for data_type in ["train"]:
+    for data_type in ["train", "dev", "test"]:
         data_dir = "./MULTIWOZ20_preprocess/"+ data_type +".json"
         data_idx = "./MULTIWOZ20_preprocess/"+ data_type +".idx"
-        
+
         output_filename = "./MULTIWOZ20_preprocess/"+ data_type +"_LLM_zero-shot_except-"+ str(except_domain) +"-domain.json"
         dataset_data = []
-        
+
         idx_lines = open(data_idx).readlines()
         test_data_lines = open(data_dir).readlines()
-        
+
         assert len(idx_lines) == len(test_data_lines)
-        
+
         none_sample_number = len(idx_lines) - activate_number
         none_sample_select_ratio = round((activate_number/none_sample_number),3)
         print(f"从none的样本中，以{none_sample_select_ratio}的概率进行挑选，从而保证二者的数量均衡")
@@ -107,22 +107,22 @@ if __name__ == '__main__':
             if dial_text['state'] == "NONE":
                 if random.random() > none_sample_select_ratio:
                     continue
-            
+
             original_number += 1
             item = {}
             idx_list = idx_lines[idx_].strip()
-            dial_json_n, dial_idx, turn_idx, frame_idx, d_name, s_name = idx_list.split("|||") 
+            dial_json_n, dial_idx, turn_idx, frame_idx, d_name, s_name = idx_list.split("|||")
 
             if d_name == except_domain:
-                except_number +=1 
+                except_number +=1
                 continue
 
             if random.random() > 0.1:
                 instru = "Track the state of the slot in the input dialogue."
             else:
                 instru = "Track the state of the slot <"+ d_name+ "-" + s_name +"> in the input dialogue."
-    
-            # 然后随机选择template 
+
+            # 然后随机选择template
             template_id = random.randint(1, 3)
             #template_id =3
             if template_id == 1:
@@ -134,7 +134,7 @@ if __name__ == '__main__':
             else:
                 print("error")
                 sys.exit(1)
-            
+
             item['instruction'] = instru
             item['input'] = input1
             item['output'] = output1
@@ -143,9 +143,9 @@ if __name__ == '__main__':
             dataset_data.append(item)
             #print(item)
             #break
-            
-        
+
+
         with open(output_filename, 'w') as f:
-            json.dump(dataset_data, f, indent=4)    
+            json.dump(dataset_data, f, indent=4)
     print("done.")
     print(f"原本的样本数量为：{original_number}。被删掉的样本数量为：{except_number}。最终该训练集的样本总数为：{len(dataset_data)}")
